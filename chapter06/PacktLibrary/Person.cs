5cc1f563ed1014cc07855ad153d1da6730190bb5
unknown <regawq@gmail.com>
2020-04-16 17:34:03 +0200

    Work in progress on chapter06.

---
+++ chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        public static Person Procreate(Person p1, Person p2)
+        {
+            Person baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        public static Person operator*(Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"{nameof(number)} cannot be less than 0!");                
+            }
+            return localFactorial(number);
+            int localFactorial(int localNumber)
+            {
+                if (localNumber < 1)
+                {
+                    return 1;
+                }
+                return localNumber * localFactorial(localNumber - 1);
+            }
+        }
+
+        public event EventHandler Shout;
+        public int AngerLevel;
+        public void Poke()
+        {
+            AngerLevel++;
+            if (AngerLevel >= 3)
+            {
+                Shout?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ chapter06/PeopleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Packt.Shared;
+
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person harry = new Person { Name = "Harry" };
+            Person mary = new Person { Name = "Mary" };
+            Person jill = new Person { Name = "Jill" };
+
+            Person baby1 = mary.ProcreateWith(harry);
+            Person baby2 = Person.Procreate(harry, jill);
+            Person baby3 = harry * mary;
+
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine("{0}'s first child is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name
+            );
+
+            WriteLine($"5! is {Person.Factorial(5)}.");
+
+            harry.Shout += Harry_Shout;
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+        }
+
+        private static void Harry_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            WriteLine($"{p.Name} is this angry: {p.AngerLevel}.");
+        }
+    }
+}
