643f25a8645a26a431907749b6037ebeb5da437d
unknown <regawq@gmail.com>
2020-04-22 16:54:11 +0200

    Work in progress on chapter07.

---
+++ chapter07/AssempliesAndNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AssempliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument doc = new XDocument();
+
+            string s1 = "Hello!";
+            String s2 = "World!";
+        }
+    }
+}
